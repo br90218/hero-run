768dac3348b2c6edcc7373d6aeea27bb29337764
Yang-Che (Brian) Teng <askmeifyouwantmyusername@gmail.com>
2017-04-08 23:48:58 +0800

    Added a player control script.

---
+++ Assets/PlayerConrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConrol : MonoBehaviour
+{
+
+    [SerializeField] private int _maxHealth = 5;
+    [SerializeField] private int _maxMana = 10;
+
+    public int MaxHealth
+    {
+        get { return _maxHealth; }
+        set { _maxHealth = value; }
+    }
+
+    public int MaxMana
+    {
+        get { return _maxMana; }
+        set { _maxMana = value; }
+    }
+
+
+    public int Health { get; private set; }
+    public int Mana { get; private set; }
+
+
+
+    // Use this for initialization
+    private void Start ()
+	{
+	    Health = _maxHealth;
+	    Mana = _maxMana;
+
+	}
+	
+	// Update is called once per frame
+    public void Update () {
+		
+	}
+
+    public void PlayerDamage(int damage)
+    {
+        Health -= damage;
+    }
+    
+}
