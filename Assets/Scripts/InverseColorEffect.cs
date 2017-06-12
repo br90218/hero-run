8024c2465dd0a0378d83fb4925749142d9cfdac4
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-12 21:23:09 +0800

    Adjusted / added some more mysterious image effects...

--- Assets/Scripts/InverseColorEffect.cs
+++ Assets/Scripts/InverseColorEffect.cs
@@ -2,28 +2,20 @@
 using System.Collections;
 
 public class InverseColorEffect: MonoBehaviour {
-	public Shader m_Shader = null;
-	private Material m_Material;
+	[SerializeField] private Material m_Material;
 
 	void Start()
 	{  
-		if (m_Shader)
+		if (!m_Material)
 		{
-			m_Material = new Material(m_Shader);
-			m_Material.name = "ImageEffectMaterial";
-			m_Material.hideFlags = HideFlags.HideAndDontSave;
-		}
-
-		else
-		{
-			Debug.LogWarning(gameObject.name + ": Shader is not assigned. Disabling image effect.", this.gameObject);
+			Debug.LogWarning(gameObject.name + ": Material is not assigned. Disabling image effect.", this.gameObject);
 			enabled = false;
 		}
 	}
 
 	void OnRenderImage(RenderTexture src, RenderTexture dst)
 	{
-		if (m_Shader && m_Material)
+		if (m_Material)
 		{
 			Graphics.Blit(src, dst, m_Material);
 		}
@@ -35,12 +27,5 @@ public class InverseColorEffect: MonoBehaviour {
 			enabled = false;
 		}
 	}
-
-	void OnDisable()
-	{
-		if (m_Material)
-		{
-			DestroyImmediate(m_Material);
-		}
-	}
+		
 }
