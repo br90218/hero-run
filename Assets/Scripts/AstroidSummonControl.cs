efb4268b3c24b66fe4ddb3a49e46b2cf57b26ad7
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-13 01:45:33 +0800

    Added Magic FX. Attempted to configure multi-display but failed.

---
+++ Assets/Scripts/AstroidSummonControl.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidSummonControl : MonoBehaviour {
+    public GameObject Controller;
+
+    private SteamVR_TrackedObject _trackedObj;
+    private bool _magicAvailable;
+    private SteamVR_LaserPointer _pointer;
+    private bool _pointerInitialized = false;
+    private bool _aiming = false;
+    private Transform _magicSign;
+    void Start()
+    {
+        _magicAvailable = true;
+        deactivateAllChilds();
+        _magicSign = this.transform.FindChild("magicSign");
+    }
+
+    private void Awake()
+    {
+        _trackedObj = Controller.GetComponent<SteamVR_TrackedObject>();
+        _pointer = Controller.GetComponent<SteamVR_LaserPointer>();
+        if (_pointer == null)
+            _pointer = Controller.AddComponent<SteamVR_LaserPointer>() as SteamVR_LaserPointer;
+    }
+
+
+    void FixedUpdate ()
+    {
+        if (!_pointerInitialized && Controller.transform.FindChild("New Game Object") != null)
+        {
+            Controller.transform.FindChild("New Game Object").gameObject.SetActive(false);
+            _pointerInitialized = true;
+        }
+        
+        var device = SteamVR_Controller.Input((int)_trackedObj.index);
+
+        if (_magicAvailable == true && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            _magicSign.gameObject.SetActive(true);
+            Controller.transform.FindChild("New Game Object").gameObject.SetActive(true);
+            _magicAvailable = false;
+            _aiming = true;
+        }
+
+        if (_aiming == true && device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Ray ray = new Ray(Controller.transform.position, Controller.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                _magicSign.position = hit.point;
+                _magicSign.up = hit.normal;
+            }
+        }
+
+            if (_aiming == true && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            _aiming = false;
+            Controller.transform.FindChild("New Game Object").gameObject.SetActive(false);            
+            StartCoroutine(shootMagic());            
+        }
+    }
+
+    IEnumerator shootMagic()
+    {
+        Ray ray = new Ray(Controller.transform.position, Controller.transform.forward);
+        RaycastHit hit;        
+        if (Physics.Raycast(ray, out hit))
+        {
+            this.transform.position = hit.point + new Vector3(0f, 15f, 0f) + Random.insideUnitSphere * 7;
+
+            this.transform.LookAt(hit.point);
+            Vector3 rot = this.transform.rotation.eulerAngles;
+            rot = new Vector3(rot.x + 180, rot.y, rot.z);
+            this.transform.rotation = Quaternion.Euler(rot);
+            _magicSign.position = hit.point;
+            _magicSign.up = hit.normal;
+            activateAllChilds();
+            yield return new WaitForSeconds(3.5f);
+            deactivateAllChilds();
+            _magicAvailable = true;
+        }
+        yield return null;
+    }
+
+
+
+    private void deactivateAllChilds()
+    {
+        for (int i = 0; i <= 6; ++i)
+        {
+            this.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    private void activateAllChilds()
+    {
+        for (int i = 0; i <= 5; ++i)
+        {
+            this.gameObject.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/FireBallControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallControl : MonoBehaviour {
+    public GameObject Controller;
+    public GameObject ActivatedGameObject;
+
+    private SteamVR_TrackedObject _trackedObj;
+    private bool _magicAvailable;
+	
+	void Start () {
+        _magicAvailable = true;
+        deactivateAllChilds();
+    }
+
+    private void Awake()
+    {
+        _trackedObj = Controller.GetComponent<SteamVR_TrackedObject>();   
+    }
+
+    void Update () {
+        var device = SteamVR_Controller.Input((int)_trackedObj.index);
+        if (_magicAvailable == true && device.GetTouchDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            activeAllChilds();
+            this.transform.parent = Controller.transform;
+            this.transform.localPosition = new Vector3(0f, 0f, 0f);
+            this.transform.localRotation = Quaternion.identity;
+            _magicAvailable = false;
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            deactivateAllChilds();
+            this.transform.parent = null;
+            StartCoroutine(shootMagic());
+        }
+    }
+
+    private void deactivateAllChilds()
+    {
+        for (int i = 0; i <= 1; ++i)
+        {
+            this.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    private void activeAllChilds()
+    {
+        for (int i = 0; i <= 1; ++i)
+        {
+            this.gameObject.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator shootMagic()
+    {
+        ActivatedGameObject.SetActive(true);
+        yield return new WaitForSeconds(3.5f);
+        ActivatedGameObject.SetActive(false);
+        _magicAvailable = true;
+    }
+}
