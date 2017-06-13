751aa30e3fa1c59c4ca2b56a3cf07d4e9385603a
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-13 14:28:02 +0800

    Modified and completed BASIC respawning system

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+	[SerializeField] private RespawnController _respawnController;
+	[SerializeField] private int _checkpointNumber;
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag ("PCPlayer")) {
+			_respawnController.SetCheckpointFlag (_checkpointNumber);
+		}
+	}
+}
