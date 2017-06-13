3c43a108923a6fdd8f8efaeefa782c65beea3a0d
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-14 04:27:45 +0800

    Effects are mostly done, but the functionality (freezing of controls) are not done yet.

--- Assets/Scripts/InverseColorEffect.cs
+++ Assets/Scripts/InverseColorEffect.cs
@@ -1,29 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InverseColorEffect: MonoBehaviour {
+public class InverseColorEffect: MonoBehaviour
+{
 	[SerializeField] private Material m_Material;
 
-	void Start()
+	public float ControlValue;
+
+	void Start ()
 	{  
-		if (!m_Material)
-		{
-			Debug.LogWarning(gameObject.name + ": Material is not assigned. Disabling image effect.", this.gameObject);
+		//ControlValue = 1f;
+		if (!m_Material) {
+			Debug.LogWarning (gameObject.name + ": Material is not assigned. Disabling image effect.", this.gameObject);
 			enabled = false;
 		}
 	}
 
-	void OnRenderImage(RenderTexture src, RenderTexture dst)
+	void OnRenderImage (RenderTexture src, RenderTexture dst)
 	{
-		if (m_Material)
-		{
-			Graphics.Blit(src, dst, m_Material);
-		}
+		if (m_Material) {
+			m_Material.SetFloat ("_InvertMultiplier", ControlValue);
+			Graphics.Blit (src, dst, m_Material);
 
-		else
-		{
-			Graphics.Blit(src, dst);
-			Debug.LogWarning(gameObject.name + ": Shader is not assigned. Disabling image effect.", this.gameObject);
+		} else {
+			Graphics.Blit (src, dst);
+			Debug.LogWarning (gameObject.name + ": Shader is not assigned. Disabling image effect.", this.gameObject);
 			enabled = false;
 		}
 	}
---
+++ Assets/Scripts/VoidBallEffect.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidBallEffect : MonoBehaviour
+{
+
+	[SerializeField] private Transform _targetTransform;
+	[SerializeField] private Vector3 _offset;
+	[SerializeField] private GameObject _voidBall;
+	[SerializeField] private AudioClip _voidBallSound;
+	[SerializeField] private AnimationCurve _scaleCurve;
+	[SerializeField] private float _growTime;
+	[SerializeField] private float _additiveNoiseFactor;
+	[SerializeField] private float _blackoutTime;
+
+
+	private float _time;
+	private GameObject _voidBallInstance;
+	private Vector3 _currScale;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke ("Activate", 10f);
+	}
+
+	private void Update ()
+	{
+		_time += Time.deltaTime;
+	}
+
+	public void Activate ()
+	{
+		_time = 0f;
+		_voidBallInstance = Instantiate (_voidBall, _targetTransform.position + _offset, Quaternion.identity, null);
+		GetComponent<AudioSource> ().clip = _voidBallSound;
+		GetComponent<AudioSource> ().Play ();
+		_voidBallInstance.transform.localScale = _currScale;
+		StartCoroutine ("BallGrow");
+		Invoke ("Blackout", _growTime);
+	}
+
+	private IEnumerator BallGrow ()
+	{
+		while (_time < _growTime) {
+			var evaluated = _scaleCurve.Evaluate (_time);
+			_currScale = new Vector3 (evaluated, evaluated, evaluated);
+			var noise = Random.insideUnitSphere * _additiveNoiseFactor;
+			_voidBallInstance.transform.localScale = _currScale + noise;
+			yield return new WaitForFixedUpdate ();
+		}
+	}
+
+	private void Blackout ()
+	{
+		Destroy (_voidBallInstance);
+		_targetTransform.gameObject.GetComponent<InverseColorEffect> ().ControlValue = 0f;
+		//TODO: Disable all controls for VR
+		//TODO: Invoke UI script
+		Invoke ("EndEffect", _blackoutTime);
+	}
+
+	private void EndEffect ()
+	{
+		_targetTransform.gameObject.GetComponent<InverseColorEffect> ().ControlValue = 1f;
+		print ("Void effect out");
+	}
+
+}
