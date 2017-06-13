ab00fd1cc0a19976576add671c96bd4f0d884804
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-13 16:46:00 +0800

    Multiscreen support now online

---
+++ Assets/Scripts/VRManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class VRManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		UnityEngine.VR.VRSettings.showDeviceView = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
