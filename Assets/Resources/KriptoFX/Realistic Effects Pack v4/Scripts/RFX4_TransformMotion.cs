4aef74e905120dccfd2d7bd5b0cb5f45f12116a8
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-13 15:41:31 +0800

    Added explosion behaviour to Effect15 but not tested out yet.

--- Assets/Resources/KriptoFX/Realistic Effects Pack v4/Scripts/RFX4_TransformMotion.cs
+++ Assets/Resources/KriptoFX/Realistic Effects Pack v4/Scripts/RFX4_TransformMotion.cs
@@ -5,170 +5,180 @@ using System.Collections.Generic;
 
 public class RFX4_TransformMotion : MonoBehaviour
 {
-    public float Distance = 30;
-    public float Speed = 1;
-    public float Dampeen = 0;
-    public float MinSpeed = 1;
-    public float TimeDelay = 0;
-    public LayerMask CollidesWith = ~0;
+	public float Distance = 30;
+	public float Speed = 1;
+	public float Dampeen = 0;
+	public float MinSpeed = 1;
+	public float TimeDelay = 0;
+	public LayerMask CollidesWith = ~0;
    
-    public GameObject[] EffectsOnCollision;
-    public float CollisionOffset = 0;
-    public float DestroyTimeDelay = 5;
-    public bool CollisionEffectInWorldSpace = true;
-    public GameObject[] DeactivatedObjectsOnCollision;
-    [HideInInspector] public float HUE = -1;
-    [HideInInspector] public List<GameObject> CollidedInstances; 
+	public GameObject[] EffectsOnCollision;
+	public float CollisionOffset = 0;
+	public float DestroyTimeDelay = 5;
+	public bool CollisionEffectInWorldSpace = true;
+	public GameObject[] DeactivatedObjectsOnCollision;
+	[HideInInspector] public float HUE = -1;
+	[HideInInspector] public List<GameObject> CollidedInstances;
 
-    private Vector3 startPositionLocal;
-    Transform t;
-    private Vector3 oldPos;
-    private bool isCollided;
-    private bool isOutDistance;
-    private Quaternion startQuaternion;
-    private float currentSpeed;
-    private float currentDelay;
-    private const float RayCastTolerance = 0.3f;
-    private bool isInitialized;
-    private bool dropFirstFrameForFixUnityBugWithParticles;
-    public event EventHandler<RFX4_CollisionInfo> CollisionEnter;
+	public float _explosionPower = 10f;
+	public float _explosionRadius = 5f;
+	public float _upwardMod = 1f;
 
-    void Start()
-    {
-        t = transform;
-        startQuaternion = t.rotation;
-        startPositionLocal = t.localPosition;
-        oldPos = t.TransformPoint(startPositionLocal);
-        Initialize();
-        isInitialized = true;
-    }
+	private Vector3 startPositionLocal;
+	Transform t;
+	private Vector3 oldPos;
+	private bool isCollided;
+	private bool isOutDistance;
+	private Quaternion startQuaternion;
+	private float currentSpeed;
+	private float currentDelay;
+	private const float RayCastTolerance = 0.3f;
+	private bool isInitialized;
+	private bool dropFirstFrameForFixUnityBugWithParticles;
 
-    void OnEnable()
-    {
-        if (isInitialized) Initialize();
-    }
+	public event EventHandler<RFX4_CollisionInfo> CollisionEnter;
 
-    void OnDisable()
-    {
-        if (isInitialized) Initialize();
-    }
+	void Start ()
+	{
+		t = transform;
+		startQuaternion = t.rotation;
+		startPositionLocal = t.localPosition;
+		oldPos = t.TransformPoint (startPositionLocal);
+		Initialize ();
+		isInitialized = true;
+	}
 
-    private void Initialize()
-    {
-        isCollided = false;
-        isOutDistance = false;
-        currentSpeed = Speed;
-        currentDelay = 0;
-        startQuaternion = t.rotation;
-        t.localPosition = startPositionLocal;
-        oldPos = t.TransformPoint(startPositionLocal);
-        OnCollisionDeactivateBehaviour(true);
-        dropFirstFrameForFixUnityBugWithParticles = true;
+	void OnEnable ()
+	{
+		if (isInitialized)
+			Initialize ();
+	}
+
+	void OnDisable ()
+	{
+		if (isInitialized)
+			Initialize ();
+	}
+
+	private void Initialize ()
+	{
+		isCollided = false;
+		isOutDistance = false;
+		currentSpeed = Speed;
+		currentDelay = 0;
+		startQuaternion = t.rotation;
+		t.localPosition = startPositionLocal;
+		oldPos = t.TransformPoint (startPositionLocal);
+		OnCollisionDeactivateBehaviour (true);
+		dropFirstFrameForFixUnityBugWithParticles = true;
       
-    }
+	}
 
-    void Update()
-    {
-        if (!dropFirstFrameForFixUnityBugWithParticles)
-        {
-            UpdateWorldPosition();
-        }
-        else dropFirstFrameForFixUnityBugWithParticles = false;
-    }
+	void Update ()
+	{
+		if (!dropFirstFrameForFixUnityBugWithParticles) {
+			UpdateWorldPosition ();
+		} else
+			dropFirstFrameForFixUnityBugWithParticles = false;
+	}
 
-    void UpdateWorldPosition()
-    {
-        currentDelay += Time.deltaTime;
-        if (currentDelay < TimeDelay)
-            return;
+	void UpdateWorldPosition ()
+	{
+		currentDelay += Time.deltaTime;
+		if (currentDelay < TimeDelay)
+			return;
 
-        var frameMoveOffset = Vector3.zero;
-        var frameMoveOffsetWorld = Vector3.zero;
-        if (!isCollided && !isOutDistance)
-        {
-            currentSpeed = Mathf.Clamp(currentSpeed - Speed*Dampeen*Time.deltaTime, MinSpeed, Speed);
-            var currentForwardVector = Vector3.forward*currentSpeed*Time.deltaTime;
-            frameMoveOffset = t.localRotation*currentForwardVector;
-            frameMoveOffsetWorld = startQuaternion*currentForwardVector;
-        }
+		var frameMoveOffset = Vector3.zero;
+		var frameMoveOffsetWorld = Vector3.zero;
+		if (!isCollided && !isOutDistance) {
+			currentSpeed = Mathf.Clamp (currentSpeed - Speed * Dampeen * Time.deltaTime, MinSpeed, Speed);
+			var currentForwardVector = Vector3.forward * currentSpeed * Time.deltaTime;
+			frameMoveOffset = t.localRotation * currentForwardVector;
+			frameMoveOffsetWorld = startQuaternion * currentForwardVector;
+		}
 
-        var currentDistance = (t.localPosition + frameMoveOffset - startPositionLocal).magnitude;
+		var currentDistance = (t.localPosition + frameMoveOffset - startPositionLocal).magnitude;
 
-        RaycastHit hit;
-        if (!isCollided && Physics.Raycast(t.position, t.forward, out hit, 10, CollidesWith))
-        {
-            if (frameMoveOffset.magnitude + RayCastTolerance > hit.distance)
-            {
-                isCollided = true;
-                t.position = hit.point;
-                oldPos = t.position;
-                OnCollisionBehaviour(hit);
-                OnCollisionDeactivateBehaviour(false);
-                return;
-            }
-        }
+		RaycastHit hit;
+		if (!isCollided && Physics.Raycast (t.position, t.forward, out hit, 10, CollidesWith)) {
+			if (frameMoveOffset.magnitude + RayCastTolerance > hit.distance) {
+				isCollided = true;
+				t.position = hit.point;
+				oldPos = t.position;
+				OnCollisionBehaviour (hit);
+				OnCollisionDeactivateBehaviour (false);
+				return;
+			}
+		}
 
-        if (!isOutDistance && currentDistance > Distance)
-        {
-            isOutDistance = true;
-            t.localPosition = startPositionLocal + t.localRotation*Vector3.forward*Distance;
-            oldPos = t.position;
-            return;
-        }
+		if (!isOutDistance && currentDistance > Distance) {
+			isOutDistance = true;
+			t.localPosition = startPositionLocal + t.localRotation * Vector3.forward * Distance;
+			oldPos = t.position;
+			return;
+		}
 
-        t.position = oldPos + frameMoveOffsetWorld;
-        oldPos = t.position;
-    }
+		t.position = oldPos + frameMoveOffsetWorld;
+		oldPos = t.position;
+	}
 
 
 
-    void OnCollisionBehaviour(RaycastHit hit)
-    {
-        var handler = CollisionEnter;
-        if (handler != null)
-            handler(this, new RFX4_CollisionInfo {Hit = hit});
-        CollidedInstances.Clear();
-        foreach (var effect in EffectsOnCollision)
-        {
-            var instance = Instantiate(effect, hit.point + hit.normal * CollisionOffset, new Quaternion()) as GameObject;
-            CollidedInstances.Add(instance);
-            if (HUE > -0.9f)
-            {  
-                RFX4_ColorHelper.ChangeObjectColorByHUE(instance, HUE);
-            }
-            instance.transform.LookAt(hit.point + hit.normal + hit.normal * CollisionOffset);
-            if (!CollisionEffectInWorldSpace) instance.transform.parent = transform;
-            Destroy(instance, DestroyTimeDelay);
-        }
-    }
+	void OnCollisionBehaviour (RaycastHit hit)
+	{
+		var handler = CollisionEnter;
+		if (handler != null)
+			handler (this, new RFX4_CollisionInfo { Hit = hit });
+		CollidedInstances.Clear ();
+		foreach (var effect in EffectsOnCollision) {
+			var instance = Instantiate (effect, hit.point + hit.normal * CollisionOffset, new Quaternion ()) as GameObject;
+			CollidedInstances.Add (instance);
+			if (HUE > -0.9f) {  
+				RFX4_ColorHelper.ChangeObjectColorByHUE (instance, HUE);
+			}
+			instance.transform.LookAt (hit.point + hit.normal + hit.normal * CollisionOffset);
+			if (!CollisionEffectInWorldSpace)
+				instance.transform.parent = transform;
+			Destroy (instance, DestroyTimeDelay);
+		}
 
-    void OnCollisionDeactivateBehaviour(bool active)
-    {
-        foreach (var effect in DeactivatedObjectsOnCollision)
-        {
-            effect.SetActive(active);
-        }
-    }
+		var explosionPoint = hit.point;
+		var colliders = Physics.OverlapSphere (explosionPoint, _explosionRadius);
+		foreach (Collider hitCollider in colliders) {
+			var rb = hitCollider.GetComponent<Rigidbody> ();
+			if (rb != null) {
+				rb.AddExplosionForce (_explosionPower, explosionPoint, _explosionRadius, _upwardMod);
+			}
+		}
 
-    void OnDrawGizmosSelected()
-    {
-        if (Application.isPlaying)
-            return;
+	}
 
-        t = transform;
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(t.position, t.position + t.forward*Distance);
+	void OnCollisionDeactivateBehaviour (bool active)
+	{
+		foreach (var effect in DeactivatedObjectsOnCollision) {
+			effect.SetActive (active);
+		}
+	}
 
-    }
+	void OnDrawGizmosSelected ()
+	{
+		if (Application.isPlaying)
+			return;
 
-    public enum RFX4_SimulationSpace
-    {
-        Local,
-        World
-    }
+		t = transform;
+		Gizmos.color = Color.blue;
+		Gizmos.DrawLine (t.position, t.position + t.forward * Distance);
 
-    public class RFX4_CollisionInfo : EventArgs
-    {
-        public RaycastHit Hit;
-    }
+	}
+
+	public enum RFX4_SimulationSpace
+	{
+		Local,
+		World
+	}
+
+	public class RFX4_CollisionInfo : EventArgs
+	{
+		public RaycastHit Hit;
+	}
 }
