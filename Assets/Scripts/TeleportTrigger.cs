0a2855dfd89a5231f5e88b9355ffcf8afec7ddff
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-14 20:07:59 +0800

    First implementation: Added camera LookAt script (not done yet)

---
+++ Assets/Scripts/TeleportTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTrigger : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _leftController;
+	[SerializeField] private GameObject _rightController;
+
+
+	private SteamVR_TrackedObject _leftTrackedObj;
+	private SteamVR_TrackedObject _rightTrackedObj;
+
+	private void Awake ()
+	{
+		_leftTrackedObj = _leftController.GetComponent<SteamVR_TrackedObject> ();
+		_rightTrackedObj = _rightController.GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var deviceL = SteamVR_Controller.Input ((int)_leftTrackedObj.index);
+		var deviceR = SteamVR_Controller.Input ((int)_rightTrackedObj.index);
+
+	}
+}
