6a4274027d56a0c244dc52da292bb6c41d56f9ba
br90218 <askmeifyouwantmyusername@gmail.com>
2017-06-14 19:50:22 +0800

    First implementation: Added targeted tower indicator

---
+++ Assets/Scripts/TeleportTargetRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTargetRotation : MonoBehaviour
+{
+	[SerializeField] private float _degreesPerSec = 360f;
+
+	private float rotX;
+	// Update is called once per frame
+	void Update ()
+	{
+		rotX += Time.deltaTime * _degreesPerSec;
+		transform.localRotation = Quaternion.Euler (new Vector3 (rotX, 90f, 90f));
+	}
+}
